ddfc483a6f2d29602aae5e87002c3d7818a402cf
JoaoWalter193 <joaowalter193@gmail.com>
2025-11-21 21:57:32 -0300

    feat(docker): Adiçõa do dokcer-compose para POC do TCC

---
+++ backend/clientes_TCC/src/main/java/com/clientes/clientes_TCC/ClientesTccApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.clientes_TCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesTccApplication.class, args);
+	}
+
+}
---
+++ backend/clientes_TCC/src/test/java/com/clientes/clientes_TCC/ClientesTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientes.clientes_TCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
