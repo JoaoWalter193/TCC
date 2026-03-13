2cbf798ac216f2e2722e04f0438ecd5f60d52152
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-13 17:12:14 -0300

    feat(pages): criação das páginas principais

--- frontend/tcc/src/app/app.routes.ts
+++ frontend/tcc/src/app/app.routes.ts
@@ -24,4 +24,16 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./recover/recover.component').then((m) => m.RecoverComponent),
   },
+  {
+    path: 'tab4',
+    loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
+  },
+  {
+    path: 'tab5',
+    loadComponent: () => import('./tab5/tab5.page').then( m => m.Tab5Page)
+  },
+  {
+    path: 'tab6',
+    loadComponent: () => import('./tab6/tab6.page').then( m => m.Tab6Page)
+  },
 ];
