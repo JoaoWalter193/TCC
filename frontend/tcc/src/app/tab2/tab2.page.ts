a83f0e3ec37305b33a2faed49d8a3752b849ab2b
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-13 16:49:41 -0300

    feat(init): verificação na inicialização se está logado.

--- frontend/tcc/src/app/tab2/tab2.page.ts
+++ frontend/tcc/src/app/tab2/tab2.page.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonGrid, IonButton, IonIcon]
 })
 export class Tab2Page {
-
+  auth = inject(AuthService);
   constructor() {}
 
 }
