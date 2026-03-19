a4cf9149afca78d4976a1d1beba6bbe79a154c6c
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-19 19:51:38 -0300

    fix(style): arrumada a camada de sobreposição e adicionados os  elementos da tab6

--- frontend/tcc/src/app/tab6/tab6.page.ts
+++ frontend/tcc/src/app/tab6/tab6.page.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonText } from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab6',
   templateUrl: './tab6.page.html',
   styleUrls: ['./tab6.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon, IonText]
 })
-export class Tab6Page implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class Tab6Page {
+  auth = inject(AuthService);
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
 
+  constructor(private router: Router) { }
 }
