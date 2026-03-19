325bea58ecf19a784743528157eb2bbc2436511f
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-19 16:35:44 -0300

    feat(favoritos): feita a verificação de login na aba de favoritos.

--- frontend/tcc/src/app/tab5/tab5.page.ts
+++ frontend/tcc/src/app/tab5/tab5.page.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonButton, IonIcon, IonCol, IonText } from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    IonIcon,
+    IonCol,
+    IonText
+],
 })
-export class Tab5Page implements OnInit {
+export class Tab5Page {
+  auth = inject(AuthService);
+  router = inject(Router);
 
-  constructor() { }
-
-  ngOnInit() {
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
-
+  constructor() {}
 }
