45285825de6cf99fa79c26692d072c7a7850360d
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-19 16:49:48 -0300

    feat(notify): itens e verificação de login adicionados.

--- frontend/tcc/src/app/tab3/tab3.page.ts
+++ frontend/tcc/src/app/tab3/tab3.page.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonCol, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonIcon, IonCol, IonButton],
 })
 export class Tab3Page {
+  auth = inject(AuthService);
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+  router = inject(Router);
   constructor() {}
 }
