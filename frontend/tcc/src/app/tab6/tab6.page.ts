55b2c6fa1b24af60d7a0ba24d3e330e0d14bbda8
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-19 20:06:42 -0300

    fix(styles): arrumado estilos básicos

--- frontend/tcc/src/app/tab6/tab6.page.ts
+++ frontend/tcc/src/app/tab6/tab6.page.ts
@@ -1,7 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonText } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonButton, IonIcon, IonText } from '@ionic/angular/standalone';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './tab6.page.html',
   styleUrls: ['./tab6.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon, IonText]
+  imports: [IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon, IonText]
 })
 export class Tab6Page {
   auth = inject(AuthService);
