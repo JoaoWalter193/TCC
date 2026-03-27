ab96f7df0783ee8e18d225ed4ae6eba54ec27dc2
lvtrindade <lucasventurintrindade@gmail.com>
2026-03-27 15:13:23 -0300

    feat(histórico): adicionado elementos do histórico

--- frontend/tcc/src/app/historico/historico.component.ts
+++ frontend/tcc/src/app/historico/historico.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IonBackButton, IonButtons, IonHeader, IonMenuButton, IonContent } from "@ionic/angular/standalone";
+import { CardComponent } from "../components/card/card.component";
+import { PostDTO } from '../models/dto/post-dto';
 
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.component.html',
   styleUrls: ['./historico.component.scss'],
+  imports: [IonBackButton, IonButtons, IonHeader, IonMenuButton, IonContent, CardComponent],
 })
-export class HistoricoComponent  implements OnInit {
+export class HistoricoComponent {
+  postsVisualizados: PostDTO[] = [];
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
