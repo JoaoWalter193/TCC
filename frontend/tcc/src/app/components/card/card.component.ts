ced54d30e018777827e40073897783b8ed158596
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-06 17:06:33 -0300

    feat(posts): filtrar por tag adicionado

--- frontend/tcc/src/app/components/card/card.component.ts
+++ frontend/tcc/src/app/components/card/card.component.ts
@@ -1,17 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon } from "@ionic/angular/standalone";
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonIcon,
+  IonChip,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { ProposicaoDTO } from 'src/app/models/dto/proposicao-dto';
+import { TipoProposicao } from 'src/app/models/dto/tipo-proposicao-enum';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, RouterLink, IonButton, IonIcon],
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    RouterLink,
+    IonIcon,
+    IonChip,
+    IonLabel,
+  ],
 })
 export class CardComponent {
   @Input() post!: ProposicaoDTO;
   @Input() link: any[] = [];
-  constructor() { }
+  @Output() tipoFiltrado = new EventEmitter<TipoProposicao>();
 
+  constructor() {}
+
+  filtrarChip(event: Event, tipo: TipoProposicao) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.tipoFiltrado.emit(tipo);
+  }
 }
