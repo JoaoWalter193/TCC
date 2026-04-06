8941ac64995672a196d3016d79cbee59c18862ac
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-06 17:06:33 -0300

    feat(posts): filtrar por tag adicionado

--- frontend/tcc/src/app/tab2/tab2.page.ts
+++ frontend/tcc/src/app/tab2/tab2.page.ts
@@ -1,18 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonContent,
-  IonButton,
-  IonButtons,
-  IonMenuButton,
-} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonContent, IonButton, IonButtons, IonMenuButton, IonChip, IonLabel, IonIcon } from '@ionic/angular/standalone';
 
 import { AuthService } from '../services/auth.service';
 import { CardComponent } from '../components/card/card.component';
 import { Router } from '@angular/router';
 import { ProposicaoDTO } from '../models/dto/proposicao-dto';
 import { ProposicaoService } from '../services/proposicao';
+import { TipoProposicao } from '../models/dto/tipo-proposicao-enum';
 
 @Component({
   selector: 'app-tab2',
@@ -26,14 +20,22 @@ import { ProposicaoService } from '../services/proposicao';
     CardComponent,
     IonButtons,
     IonMenuButton,
-  ],
+    IonChip,
+    IonLabel,
+    IonIcon
+],
 })
 export class Tab2Page implements OnInit {
   auth = inject(AuthService);
 
   posts: ProposicaoDTO[] = [];
+  postsFiltrados: ProposicaoDTO[] = [];
+  tipoFiltroAtivo: TipoProposicao | null = null;
 
-  constructor(private router: Router, private proposicaoService: ProposicaoService) {}
+  constructor(
+    private router: Router,
+    private proposicaoService: ProposicaoService,
+  ) {}
 
   ngOnInit() {
     this.carregarPosts();
@@ -43,6 +45,7 @@ export class Tab2Page implements OnInit {
     this.proposicaoService.listar().subscribe({
       next: (data) => {
         this.posts = data;
+        this.postsFiltrados = data;
       },
       error: (err) => {
         console.error('Erro ao carregar proposições', err);
@@ -50,7 +53,20 @@ export class Tab2Page implements OnInit {
     });
   }
 
+  filtrarPorTipo(tipoProposicao: TipoProposicao) {
+    this.tipoFiltroAtivo = tipoProposicao;
+
+    this.postsFiltrados = this.posts.filter(
+      (post) => post.tipoProposicao === tipoProposicao,
+    );
+  }
+
   navigateToLogin() {
     this.router.navigate(['/login']);
+  }
+
+  limparFiltro() {
+    this.tipoFiltroAtivo = null;
+    this.postsFiltrados = this.posts;
   }
 }
