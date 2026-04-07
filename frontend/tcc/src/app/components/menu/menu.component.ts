53c8b4905713d182769ab7a6b39efe6a9e625020
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-07 17:09:24 -0300

    feat(menu): estilização básica do menu adicionada

--- frontend/tcc/src/app/components/menu/menu.component.ts
+++ frontend/tcc/src/app/components/menu/menu.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
-import {
-  IonMenu,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonList,
-  IonItem,
-  IonMenuToggle,
-} from '@ionic/angular/standalone';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonMenuToggle, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { DashboardMode } from 'src/app/services/dashboard-mode';
 
 @Component({
@@ -30,7 +21,9 @@ import { DashboardMode } from 'src/app/services/dashboard-mode';
     IonList,
     IonItem,
     IonMenuToggle,
-  ],
+    IonIcon,
+    IonLabel
+],
 })
 export class MenuComponent {
   constructor(private modoService: DashboardMode, private router: Router) {}
