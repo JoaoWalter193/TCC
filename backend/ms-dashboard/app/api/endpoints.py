887e903d39bd40aa079ff21f8241cd3423051397
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-20 20:48:13 -0300

    feat(dashboard): feita a conexão básica com o BD

--- backend/ms-dashboard/app/api/endpoints.py
+++ backend/ms-dashboard/app/api/endpoints.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
 from app.schemas.dashboard import TableRequest, TableResponse
 from app.services.pandas_engine import process_dynamic_table
+from app.services.pandas_engine import processar_ranking_vereadores
 
 router = APIRouter()
+
+@router.get("/dashboard/ranking-vereadores")
+def get_ranking_vereadores():
+    dados_processados = processar_ranking_vereadores()
+
+    return {
+        "columnDefs": [
+            {"headerName": "Vereador", "field": "vereador_nome", "sortable": True},
+            {"headerName": "Contagem", "field": "contagem"},
+            {"headerName": "Porcentagem", "field": "porcentagem"}
+        ],
+        "rowData": dados_processados
+    }
 
 @router.post("/generate-table", response_model=TableResponse)
 def generate_table(request: TableRequest):
