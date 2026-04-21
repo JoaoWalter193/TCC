533631f2c002fbfb24471a04ddc1e8ce9e24b849
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-20 21:29:23 -0300

    feat(dashboard): Feita a conexão entre o Front e o Back - ainda não passando pelo API Gateway

--- frontend/tcc/src/app/components/dashboard-view/dashboard-view.component.ts
+++ frontend/tcc/src/app/components/dashboard-view/dashboard-view.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { Dashboard } from 'src/app/services/dashboard';
+import { IonContent } from "@ionic/angular/standalone";
+import { AgGridModule } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-dashboard-view',
   templateUrl: './dashboard-view.component.html',
   styleUrls: ['./dashboard-view.component.scss'],
+  imports: [IonContent, AgGridModule],
 })
 export class DashboardViewComponent  implements OnInit {
+  columnDefs: ColDef[] = [];
+  rowData: any[] = [];
 
-  constructor() { }
+  constructor(private dashboardService: Dashboard) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadDashboard();
+  }
 
+  loadDashboard() {
+    this.dashboardService.getDashboardDefault().subscribe({
+      next: (data) => {
+        this.columnDefs = data.columnDefs;
+        this.rowData = data.rowData;
+      },
+      error: (err) => console.error('Erro ao carregar dashboard:', err)
+    })
+  }
 }
