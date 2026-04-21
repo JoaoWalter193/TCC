4eae5931f7d29308d54ef09aa888087f3a4190a3
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-21 00:08:13 -0300

    feat(dashboard): realizada a totalidade do dashboard padrão

--- backend/ms-dashboard/app/api/endpoints.py
+++ backend/ms-dashboard/app/api/endpoints.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.schemas.dashboard import TableRequest, TableResponse
-from app.services.pandas_engine import process_dynamic_table
 from app.services.pandas_engine import processar_ranking_vereadores
 
 router = APIRouter()
@@ -17,8 +16,3 @@ def get_ranking_vereadores():
         ],
         "rowData": dados_processados
     }
-
-@router.post("/generate-table", response_model=TableResponse)
-def generate_table(request: TableRequest):
-    result = process_dynamic_table(request)
-    return result
