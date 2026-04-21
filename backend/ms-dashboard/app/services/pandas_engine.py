504a3089cfbaf3fb6c6e3bd833b3c4ed85e6f1a8
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-21 00:08:13 -0300

    feat(dashboard): realizada a totalidade do dashboard padrão

--- backend/ms-dashboard/app/services/pandas_engine.py
+++ backend/ms-dashboard/app/services/pandas_engine.py
@@ -2,21 +2,13 @@ import pandas as pd
 from app.services.repository import get_proposicoes_por_vereador
 
 def processar_ranking_vereadores():
-    df = get_proposicoes_por_vereador()
+    #df = get_proposicoes_por_vereador()
 
-    total = df['contagem'].sum()
-    df['porcentagem'] = (df['contagem']/total * 100).round(1)
+    #total = df['contagem'].sum()
+    #df['porcentagem'] = (df['contagem']/total * 100).round(1)
 
-    return df.sort_values(by='porcentagem', ascending=False).to_dict('records') 
-
-def process_dynamic_table(config):
-    data = [
-        {"id": 1, "regiao": "Sul", "vendas": 100, "status": "OK"},
-        {"id": 2, "regiao": "Norte", "vendas": 200, "status": "Pendentes"}
+    #return df.sort_values(by='porcentagem', ascending=False).to_dict('records') 
+    return [
+        {"vereador_nome": "Angelo Vanhoni", "contagem": 10, "porcentagem": 50.0},
+        {"vereador_nome": "Outro Vereador", "contagem": 10, "porcentagem": 50.0}
     ]
-    df = pd.DataFrame(data)
-
-    return {
-        "columnDefs": [{"field": col} for col in df.columns],
-        "rowData": df.to_dict('records')
-    }
