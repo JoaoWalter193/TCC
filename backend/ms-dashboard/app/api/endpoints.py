8f0672878bf44fd92be8ec49033d3b3d56327053
lvtrindade <lucasventurintrindade@gmail.com>
2026-04-24 14:57:04 -0300

    feat(dashboard): mudanças para utilização do bd

--- backend/ms-dashboard/app/api/endpoints.py
+++ backend/ms-dashboard/app/api/endpoints.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from app.services import pandas_engine
+from app.core.database import get_db
 from app.schemas.dashboard import TableRequest, TableResponse
 from app.services.pandas_engine import processar_ranking_vereadores
 
 router = APIRouter()
+
+@router.get("/metadata")
+def read_metadata(db: Session = get_db()):
+    return pandas_engine.get_dashboard_metadata(db)
 
 @router.get("/dashboard/default")
 def get_ranking_vereadores():
