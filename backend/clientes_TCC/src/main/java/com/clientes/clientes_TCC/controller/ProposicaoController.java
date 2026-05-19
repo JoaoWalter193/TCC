14618240664e5e35c7d75924f707c53724886d68
JoaoWalter193 <joaowalter193@gmail.com>
2026-05-19 19:36:10 -0300

    fix: GET's proposicao

--- backend/clientes_TCC/src/main/java/com/clientes/clientes_TCC/controller/ProposicaoController.java
+++ backend/clientes_TCC/src/main/java/com/clientes/clientes_TCC/controller/ProposicaoController.java
@@ -4,16 +4,14 @@ package com.clientes.clientes_TCC.controller;
 import com.clientes.clientes_TCC.domain.Proposicao.ProposicaoEspecificaDTO;
 import com.clientes.clientes_TCC.domain.Proposicao.ProposicaoListaResponseDTO;
 import com.clientes.clientes_TCC.service.ProposicaoService;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/prop")
@@ -23,14 +21,17 @@ public class ProposicaoController {
     @Autowired
     private ProposicaoService proposicaoService;
 
+
+    @GetMapping
     public ResponseEntity<Page<ProposicaoListaResponseDTO>> listarProposicoes(
             @RequestParam(required = false) String tag,
-            @PageableDefault(size = 10, sort = "dataEnvio", direction = Sort.Direction.DESC)
+            @ParameterObject
+            @PageableDefault(size = 10, sort = "ultimoTramite", direction = Sort.Direction.DESC)
             Pageable pageable) {
 
         return proposicaoService.listarProposicoes(tag,pageable);
     }
-
+    @GetMapping("/{codigo}")
     public ResponseEntity<ProposicaoEspecificaDTO> buscarProposicao(
             @PathVariable Long codigo) {
         return proposicaoService.buscarProposicao(codigo);
