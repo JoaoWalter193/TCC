54171cbbc5f2d47cade50a327fe4ae9d1fd2acd2
lvtrindade <lucasventurintrindade@gmail.com>
2026-06-12 00:35:02 -0300

    feat(loggout): Loggout realizado.

--- frontend/tcc/src/app/configuracoes/configuracoes.component.ts
+++ frontend/tcc/src/app/configuracoes/configuracoes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { IonHeader, IonButtons, IonBackButton, IonMenuButton, IonContent, IonItem, IonList, IonToggle, IonButton, IonToolbar, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-configuracoes',
@@ -28,9 +29,17 @@ export class ConfiguracoesComponent implements OnInit {
   paletteToggle: boolean = false;
   notifyToggle: boolean = false;
 
-  constructor() {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {}
+
+  sair() {
+    this.auth.logout();
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
 
   toggleChange(event: CustomEvent) {}
 
